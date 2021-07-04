e2dd9c9c91ab3cc0bd9bedcc28ae8079ee76f9db
hideyourface <pugliatti.barbara@gmail.com>
2021-07-04 23:30:29 +0200

    (feat)rpgBase: creation of a login contants file

---
+++ src/app/core/constants/login.constants.ts
@@ -0,0 +1,12 @@
+//set this to true if you want to display the link to the manual in the login page
+export const DISPLAY_MANUAL_LINK = true;
+
+//set this to true if you want to display the social media links in the login page
+export const DISPLAY_SOCIAL_LINK = true;
+
+//here you can set the configuration for allowing the log in with a test user 
+export const ALLOW_BOT_USER = true;
+export const BOT_USER_AUTH_DATA = {
+    'username' : 'test',
+    'password' : 'pass'
+}
